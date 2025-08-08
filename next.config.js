7afe9a32b85aa906fe11242f79f04251cac46e44
simonpeter@student.telkomuniversity.ac.id <simonpeter@student.telkomuniversity.ac.id>
2025-08-08 17:07:25 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -3,9 +3,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['images.unsplash.com'],
   },
+  experimental: {
+    esmExternals: false,
+  },
+  swcMinify: false,
+  compiler: {
+    removeConsole: false,
+  },
+  output: 'standalone',
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
